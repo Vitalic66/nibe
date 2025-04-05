4723a679eaae500f2f546113116d54bce62c216d
Vitalic66 <red-dot1@web.de>
2025-04-05 09:27:35 +0200

    Create relay.py

---
+++ relay.py
@@ -0,0 +1,23 @@
+import time
+import subprocess
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)  # Beispiel: GPIO18 verwenden
+
+def is_program_running(name):
+    try:
+        subprocess.check_output(["pgrep", "-f", name])
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+try:
+    while True:
+        if is_program_running("keypress_mqtt"):
+            GPIO.output(18, GPIO.HIGH)
+        else:
+            GPIO.output(18, GPIO.LOW)
+        time.sleep(5)  # alle 5 Sekunden prüfen
+except KeyboardInterrupt:
+    GPIO.cleanup()
